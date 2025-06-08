27c8cc3761f56e0990c103743ded3ba2dd0af733
Vasyl_Pidkaminetskuy <pidkaminetskuy@gmail.com>
2025-06-09 02:01:29 +0300

    add task 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,11 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalPrice = quantity * pricePerDroid
-    
-    if (customerCredits < totalPrice) {
-        return 'Insufficient funds!'
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    }
+  let totalPrice = quantity * pricePerDroid;
 
+  if (customerCredits < totalPrice) {
+    return 'Insufficient funds!';
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
